fcaa39d917930a204aa10b69784744efeab9c3c7
Kaleb <kaleb.starr@proton.me>
2026-02-19 03:32:50 +0100

    Add mysql

--- devenv.nix
+++ devenv.nix
@@ -6,4 +6,21 @@
     zig.enable = true;
   };
 
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql80;
+    initialDatabases = [
+      { name = "mydb"; }
+    ];
+    ensureUsers = [
+      {
+        name = "devuser";
+        password = "devpass";
+        ensurePermissions = {
+          "mydb.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+
 }
